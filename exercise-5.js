ee36e2c9c8c7c1bb519210a50c23cd930219b126
alantheandroid <alessio.ilardi@gmail.com>
2022-05-20 15:39:37 +0200

    feat: add 'averageAge' function

--- exercise-5.js
+++ exercise-5.js
@@ -1,18 +1,27 @@
 function calculateAverageAge(persons) {
-  // ...
+  let allAge = [];
+  for (let i = 0; i < persons.length; i++) {
+    allAge.push(persons[i].age);
+  }
+  let sum = 0;
+  for (let i = 0; i < allAge.length; i++) {
+    sum += allAge[i];
+  }
+  let media = sum / allAge.length;
+  return `La media delle età è: ${media}`;
 }
 
 const persons = [
-  { name: 'Paul', age: 16 },
-  { name: 'George', age: 17 },
-  { name: 'Lucas', age: 21 },
-  { name: 'Marco', age: 32 },
-  { name: 'Peter', age: 18 },
-  { name: 'Carl', age: 13 },
-  { name: 'Simon', age: 24 },
-  { name: 'Mark', age: 15 },
-  { name: 'Sandra', age: 34 },
-  { name: 'Alice', age: 28 }
+  { name: "Paul", age: 16 },
+  { name: "George", age: 17 },
+  { name: "Lucas", age: 21 },
+  { name: "Marco", age: 32 },
+  { name: "Peter", age: 18 },
+  { name: "Carl", age: 13 },
+  { name: "Simon", age: 24 },
+  { name: "Mark", age: 15 },
+  { name: "Sandra", age: 34 },
+  { name: "Alice", age: 28 },
 ];
 
 const averageAge = calculateAverageAge(persons);
